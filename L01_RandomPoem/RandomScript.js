9f7f98ca3124486be39471c806a1f4b3b2cefde3
DanielMeier12 <meier.da12@gmail.com>
2022-10-15 18:05:50 +0200

    add L01

---
+++ L01_RandomPoem/RandomScript.js
@@ -0,0 +1,26 @@
+"use strict";
+/*
+Aufgabe: <L01 RandomPoem>
+Name: <Daniel Meier>
+Matrikel: <271129>
+Datum: <15.10.2022>
+Quellen: <Natan Haider, Aanya Khetarpal>
+*/
+let subject = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+let predicate = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let object = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+for (let index = 6; index >= 1; index--) {
+    let versOutput = getVerse(subject, predicate, object);
+    console.log(versOutput);
+}
+function getVerse(_subject, _predicate, _object) {
+    let randomSubject = Math.floor(Math.random() * _subject.length);
+    let randomPredicate = Math.floor(Math.random() * _predicate.length);
+    let randomObjekt = Math.floor(Math.random() * _object.length);
+    let verse = _subject[randomSubject] + " " + _predicate[randomPredicate] + " " + _object[randomObjekt];
+    _subject.splice(randomSubject, 1);
+    _predicate.splice(randomPredicate, 1);
+    _object.splice(randomObjekt, 1);
+    return verse;
+}
+//# sourceMappingURL=RandomScript.js.map
---
+++ L01_RandomPoem/RandomScript.ts
@@ -0,0 +1,30 @@
+/*
+Aufgabe: <L01 RandomPoem>
+Name: <Daniel Meier>
+Matrikel: <271129>
+Datum: <15.10.2022>
+Quellen: <Natan Haider, Aanya Khetarpal>
+*/
+
+let subject: string [] = ["Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore"];
+let predicate: string [] = ["braut", "liebt", "studiert", "hasst", "zaubert", "zerstört"];
+let object: string [] = ["Zaubertränke", "den Grimm", "Lupin", "Hogwarts", "die Karte des Rumtreibers", "Dementoren"];
+
+for (let index: number = 6; index >= 1; index--) {
+    let versOutput: string = getVerse(subject, predicate, object);
+    console.log(versOutput);
+}
+
+function getVerse (_subject: string[], _predicate: string[], _object: string[]): string { 
+    let randomSubject: number = Math.floor(Math.random() * _subject.length);
+    let randomPredicate: number = Math.floor(Math.random() * _predicate.length);
+    let randomObjekt: number = Math.floor(Math.random() * _object.length); 
+
+    let verse: string = _subject[randomSubject] + " " + _predicate[randomPredicate] + " " + _object[randomObjekt];  
+
+    _subject.splice(randomSubject, 1 );
+    _predicate.splice(randomPredicate, 1 );
+    _object.splice(randomObjekt, 1 );
+
+    return verse;
+}
